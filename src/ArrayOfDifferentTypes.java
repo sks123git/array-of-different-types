a61996ab276fc4c992881c274c6db2472261f690
subzero <shubham8111994@gmail.com>
2023-02-18 19:38:20 +0530

    added: feature to print different array types

--- src/ArrayOfDifferentTypes.java
+++ src/ArrayOfDifferentTypes.java
@@ -1,5 +1,26 @@
+//Program to print different array types
 public class ArrayOfDifferentTypes {
+    public static void print(Integer[] a){
+        for (int i:a) {
+            System.out.println(i);
+        }
+    }
+    public static void print(Double[] a){
+        for (double i:a) {
+            System.out.println(i);
+        }
+    }
+    public static void print(Character[] a){
+        for (char i:a) {
+            System.out.println(i);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] intValue= {1,2,3,4};
+        Double[] doubleValue = {1.2,3.2,4.1,1.1};
+        Character[] charValue = {'q','s','v','d'};
+        ArrayOfDifferentTypes.print(intValue);
+        ArrayOfDifferentTypes.print(doubleValue);
+        ArrayOfDifferentTypes.print(charValue);
     }
 }
