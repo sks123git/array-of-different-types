40c97c29002ffce551d052493b0a2421a1343614
subzero <shubham8111994@gmail.com>
2023-02-18 22:05:38 +0530

    added: feature to print different array types using generics and generic class

--- src/ArrayOfDifferentTypes.java
+++ src/ArrayOfDifferentTypes.java
@@ -1,6 +1,14 @@
 //Program to print different array types using Generics
-public class ArrayOfDifferentTypes {
-    public static <E> void print(E[] array){    //
+public class ArrayOfDifferentTypes<E> {
+    private E[] array;
+
+    public ArrayOfDifferentTypes(E[] array){
+        this.array=array;
+    }
+    public void toPrint(){
+        ArrayOfDifferentTypes.toPrint(this.array);
+    }
+    public static <E> void toPrint(E[] array){    //
         for (E i:array) {
             System.out.println(i);
         }
@@ -10,8 +18,8 @@ public class ArrayOfDifferentTypes {
         Integer[] intValue= {1,2,3,4};
         Double[] doubleValue = {1.2,3.2,4.1,1.1};
         Character[] charValue = {'q','s','v','d'};
-        ArrayOfDifferentTypes.print(intValue);
-        ArrayOfDifferentTypes.print(doubleValue);
-        ArrayOfDifferentTypes.print(charValue);
+        new ArrayOfDifferentTypes(intValue).toPrint();
+        ArrayOfDifferentTypes.toPrint(doubleValue);
+        ArrayOfDifferentTypes.toPrint(charValue);
     }
 }
